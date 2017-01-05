aa0a6eac3276aff14dd31e0f502575640bdc8e2a
Ethan Stark <ethanstark@gmail.com>
2017-01-04 17:23:16 -0800

    Sort function and tests

--- src/spec/sort_spec.js
+++ src/spec/sort_spec.js
@@ -4,16 +4,15 @@ import R from 'ramda'
 const expect = chai.expect
 
 describe('sort', () => {
-  it('Returns a new list, composed of n-tuples of consecutive elements!', () => {
-    var diff = function(a, b) { return a - b; };
+  it('Sorts the list in ascending order based on difference function', () => {
+    let diff = function(a, b) { return a - b }
     expect(sort(diff, [4,2,7,5]))
       .to.eql([2, 4, 5, 7])
   })
-  it('Returns a new list, composed of n-tuples of consecutive elements', () => {
-    expect(aperture(3, [1, 2, 3, 4, 5]))
-      .to.eql([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
-  })
-  it('If predicate is greater than the lenght of the array, returns empty array',
-    () => { expect(aperture(9, [1, 2, 3, 4, 5])).to.eql([])
+  it('returns a new array', () => {
+    let diff = function(a, b) { return a - b }
+    let inputArray = [2, 4, 5, 7]
+    expect(sort(diff, [2, 4, 5, 7]))
+      .not.to.equal(inputArray)
   })
 })
