07d42caff8a167147cffc2d65affdde5425e9838
Ethan Stark <ethanstark@gmail.com>
2017-01-05 14:45:59 -0800

    Binary function and tests

---
+++ src/spec/binary_spec.js
@@ -0,0 +1,14 @@
+import chai from 'chai'
+import binary from '../lib/binary.js'
+import R from 'ramda'
+const expect = chai.expect
+
+describe('Binary', () => {
+  it('wraps a ternary function such that it only accepts 2 arguments', () => {
+    let takesThreeArgs = (a, b, c) => [a, b, c]
+    let takesTwoArgs = binary(takesThreeArgs)
+    let twoArgs = takesTwoArgs( 1, 2, 3 )
+    expect( takesTwoArgs.length ).to.eql( 2 )
+    expect( takesTwoArgs( 1, 2, 3 ) ).to.eql( [ 1, 2, undefined ] )
+  })
+})
