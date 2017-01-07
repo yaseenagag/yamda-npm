f6a006de2989f3d41e3d18a46f5d0eeb33f83a82
yaseenagag <yaseen.uc@gmail.com>
2017-01-06 17:42:23 -0800

    Assoc function and tests

---
+++ src/spec/assoc_spec.js
@@ -0,0 +1,13 @@
+import chai from 'chai'
+import assoc from '../lib/assoc.js'
+import R from 'ramda'
+const expect = chai.expect
+
+describe('Assoc', () => {
+  it('Accepts a key and a value, and adds it to a clone of { 3rd arg }', () => {
+    expect( assoc('c', 3, {a: 1, b: 2})).to.eql( {a: 1, b: 2, c: 3} )
+  })
+  it('Accepts an array as a first argument', () => {
+    expect( assoc([4], 5, {a: 1, b: 2})).to.eql( {a: 1, b: 2, 4: 5} )
+  })
+})
