305c288a165b6f035a5a8ef537b38ddb98dbb11c
yaseenagag <yaseen.uc@gmail.com>
2017-01-11 21:13:54 -0800

    test updates

--- src/spec/binary_spec.js
+++ src/spec/binary_spec.js
@@ -11,4 +11,20 @@ describe('Binary', () => {
     expect( takesTwoArgs.length ).to.eql( 2 )
     expect( takesTwoArgs( 1, 2, 3 ) ).to.eql( [ 1, 2, undefined ] )
   })
+
+  it('wraps a ternary function such that it only accepts 2 arguments', () => {
+    let takesThreeArgs = (a, b, c) => [a, b, c]
+    let takesTwoArgs = binary(takesThreeArgs)
+    let twoArgs = takesTwoArgs( 1, 2, 3 )
+    expect( takesTwoArgs.length ).to.eql( 2 )
+    expect( takesTwoArgs( 1, 2, 3 ) ).to.eql( [ 1, 2, undefined ] )
+  })
+
+  it('wraps a ternary function such that it only accepts 2 arguments', () => {
+    let takesThreeArgs = (a, b, c) => [a, b, c]
+    let takesTwoArgs = binary(takesThreeArgs)
+    let twoArgs = takesTwoArgs( 1, 2, 3 )
+    expect( takesTwoArgs.length ).to.eql( 2 )
+    expect( takesTwoArgs( 1, 2, 3 ) ).to.eql( [ 1, 2, undefined ] )
+  })
 })
